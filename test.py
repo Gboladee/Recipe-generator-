ec563ae2f75431d71330f9ade32856d7a791cfe3
gboladee <gboladeadeniji@gmail.com>
2022-11-15 00:33:18 +0100

    o

---
+++ test.py
@@ -0,0 +1,8 @@
+#Well - let's come up with a recipe generator to build us an amazing dish for today's evening meal!
+#Remember when you were a kid and thought the ideal dinner would just be all your favorite things mixed in a bowl? well this is program is a recipe generator that helps create an amazing dish!
+Favfood = input("what is your favorite meal? ")
+FavPlant = input ("guess a type of plant? ")
+method= input("Tell me a method of cooking you know. ")
+burntfood = input("How do you say 'burnt food' in your language? ")
+item = input("Tell me a household item ")
+print(Favfood ,  FavPlant ,  method , burntfood , item)
